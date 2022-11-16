8b5597cf441d2fbbc49b521930f5c06bab04ab0a
cmay4 <cmay4@smail.uni-koeln.de>
2022-11-16 14:17:45 +0100

    some changes for pick_card()

--- Exercise6/src/BetMore_Game/BetMore_Game.java
+++ Exercise6/src/BetMore_Game/BetMore_Game.java
@@ -2,20 +2,18 @@ package BetMore_Game;
 
 public class BetMore_Game {
 	
-	int x;
 	static Player computer;
 	Player player_2;
-	Shuffle s;
 	Player winner;
 	
 	public BetMore_Game(Player p1, Player p2) {
 		computer = p1;
 		player_2 = p2;
-		s = new Shuffle();
 	}
 	
 	public static void play() {
 		System.out.println(computer+" is playing...");
+		
 		
 	}
 
--- Exercise6/src/BetMore_Game/Shuffle.java
+++ Exercise6/src/BetMore_Game/Shuffle.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Shuffle {
 
 	static int times=0;;
-	static Scanner s = new Scanner(System.in);
+	
 	
 	public Shuffle() {
 		
@@ -14,6 +14,7 @@ public class Shuffle {
 	
 	public int pick_card(Player p) {
 		Random r = new Random();
+		Scanner s = new Scanner(System.in);
 		
 		if(p.getName()=="Computer") {
 			p.x = r.nextInt(100)+1;
@@ -23,7 +24,7 @@ public class Shuffle {
 			return p.x;
 		}
 		else if(p.getName()=="Player_2" ){
-			while(p.user_selected==false && times <=5){
+			while(p.user_selected==false && times <5){
 				p.x = r.nextInt(100)+1;
 				times++;
 				System.out.println("Would you like to pick this number "+p.x+" ? - y='YES', n='NO'");
@@ -32,6 +33,7 @@ public class Shuffle {
 				}
 			}
 			System.out.println("Your number is selected!");
+			s.close();
 			return p.x;
 		}
 		return 0;
